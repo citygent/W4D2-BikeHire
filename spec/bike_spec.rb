485f03338771672f1df1f7faba77069e9e424cef
James White <cyberyt@gmail.com>
2015-09-01 12:43:37 +0100

    refactored a bit

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,20 +1,21 @@
 require_relative '../lib/bike.rb'
 
 #describing functionlity of the specific bike class
+
 describe Bike do 
-  it 'should not be broken after we create it' do 
-    bike = Bike.new #instantiate an instance of the Bike Class
+
+  let(:bike) {Bike.new} #instantiate an instance of the Bike Class every test. 
+
+  it 'should not be broken after we create it' do
     expect(bike.broken?).to be false
   end
 
   it 'should be able to break down' do  
-    bike = Bike.new
     bike.break
     expect(bike.broken?).to be true
   end
 
   it 'should be able to be fixed' do
-    bike = Bike.new
     bike.break
     bike.fix
     expect(bike.broken?).to be false  
